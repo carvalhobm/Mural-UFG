de01115ae3b6c90da96d0f0124c42c21161dc08d
douglasspgyn <dsp_nike@hoitmail.com>
2016-02-24 21:04:53 -0300

    [SonarQube]

--- MuralUFG/app/src/main/java/br/ufg/inf/muralufg/activity/InboxActivity.java
+++ MuralUFG/app/src/main/java/br/ufg/inf/muralufg/activity/InboxActivity.java
@@ -20,20 +20,16 @@ import java.io.IOException;
 import br.ufg.inf.muralufg.fragment.InboxFragment;
 import br.ufg.inf.muralufg.fragment.NavigationDrawerFragment;
 import br.ufg.inf.muralufg.R;
+import br.ufg.inf.muralufg.utils.gcm.RegisterGCM;
 import br.ufg.inf.muralufg.utils.mail.SendEmailTask;
 
 
 public class InboxActivity extends AbstractBaseActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    private static final String TAG = "InboxActivity";
-
-    private static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private Context context;
-    private String senderID = "439472309664";
-    private GoogleCloudMessaging gcm;
-    private String regid;
+    public static GoogleCloudMessaging gcm;
+    public static String regid;
+    public static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +45,10 @@ public class InboxActivity extends AbstractBaseActivity implements NavigationDra
 
         if (checkPlayServices()) {
             gcm = GoogleCloudMessaging.getInstance(context);
-            regid = getRegistrationId(context);
+            regid = RegisterGCM.getRegistrationId(context);
 
             if (regid.isEmpty()) {
-                registerInBackground();
+                RegisterGCM.registerInBackground(context);
             }
         }
     }
@@ -94,75 +90,5 @@ public class InboxActivity extends AbstractBaseActivity implements NavigationDra
             return false;
         }
         return true;
-    }
-
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager()
-                    .getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
-
-    private void storeRegistrationId(Context context, String regId) {
-        final SharedPreferences prefs = getGcmPreferences();
-        int appVersion = getAppVersion(context);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_REG_ID, regId);
-        editor.putInt(PROPERTY_APP_VERSION, appVersion);
-        editor.commit();
-    }
-
-    private String getRegistrationId(Context context) {
-
-        final SharedPreferences prefs = getGcmPreferences();
-
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.isEmpty()) {
-            return "";
-        }
-
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            return "";
-        }
-        return registrationId;
-    }
-
-    private void registerInBackground() {
-
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                String msg = "";
-                try {
-                    if (gcm == null) {
-                        gcm = GoogleCloudMessaging.getInstance(context);
-                    }
-                    regid = gcm.register(senderID);
-                    msg = "Device registered, registration ID=" + regid;
-
-                    sendRegistrationIdToBackend(regid);
-
-                    storeRegistrationId(context, regid);
-                } catch (IOException ex) {
-                    Log.e(InboxActivity.TAG, ex.getMessage());
-                    msg = "Error :" + ex.getMessage();
-                }
-                return msg;
-            }
-        }.execute(null, null, null);
-    }
-
-    private SharedPreferences getGcmPreferences() {
-        return getSharedPreferences(InboxActivity.class.getSimpleName(), Context.MODE_PRIVATE);
-    }
-
-    private void sendRegistrationIdToBackend(final String emailBody) {
-
-        new SendEmailTask().execute(emailBody);
     }
 }
---
+++ MuralUFG/app/src/main/java/br/ufg/inf/muralufg/utils/gcm/RegisterGCM.java
@@ -0,0 +1,92 @@
+package br.ufg.inf.muralufg.utils.gcm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+
+import br.ufg.inf.muralufg.activity.InboxActivity;
+import br.ufg.inf.muralufg.utils.mail.SendEmailTask;
+
+public class RegisterGCM {
+    private static final String TAG = "RegisterGCM";
+
+    private static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static String senderID = "439472309664";
+
+    public static String getRegistrationId(Context context) {
+
+        final SharedPreferences prefs = getGcmPreferences(context);
+
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            return "";
+        }
+
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            return "";
+        }
+        return registrationId;
+    }
+
+    private static SharedPreferences getGcmPreferences(Context context) {
+        return context.getSharedPreferences((InboxActivity.class.getSimpleName()), Context.MODE_PRIVATE);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    public static void registerInBackground(final Context context) {
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (InboxActivity.gcm == null) {
+                        InboxActivity.gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    InboxActivity.regid = InboxActivity.gcm.register(senderID);
+                    msg = "Device registered, registration ID=" + InboxActivity.regid;
+
+                    sendRegistrationIdToBackend(InboxActivity.regid);
+
+                    storeRegistrationId(context, InboxActivity.regid);
+                } catch (IOException ex) {
+                    Log.e(TAG, ex.getMessage());
+                    msg = "Error :" + ex.getMessage();
+                }
+                return msg;
+            }
+        }.execute(null, null, null);
+    }
+
+    private static void sendRegistrationIdToBackend(final String emailBody) {
+        new SendEmailTask().execute(emailBody);
+    }
+
+    private static void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        int appVersion = getAppVersion(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+}
