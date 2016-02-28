0afbc8a54783c2e0655a7ed06aedc4a191784c30
Bruno Martins de Carvalho <bruno_28_4@hotmail.com>
2016-02-28 18:08:19 -0300

    Criação das classes de testes unitários.

---
+++ MuralUFG/app/src/test/java/br/ufg/inf/muralufg/model/AcademicUnitsTest.java
@@ -0,0 +1,57 @@
+package br.ufg.inf.muralufg.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bruno on 28/02/2016.
+ */
+public class AcademicUnitsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testSetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUnitID() throws Exception {
+
+    }
+
+    @Test
+    public void testSetUnitID() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUnit() throws Exception {
+
+    }
+
+    @Test
+    public void testSetUnit() throws Exception {
+
+    }
+
+    @Test
+    public void testGetIsChecked() throws Exception {
+
+    }
+
+    @Test
+    public void testSetIsChecked() throws Exception {
+
+    }
+}
---
+++ MuralUFG/app/src/test/java/br/ufg/inf/muralufg/model/DrawerItemTest.java
@@ -0,0 +1,27 @@
+package br.ufg.inf.muralufg.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bruno on 28/02/2016.
+ */
+public class DrawerItemTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetItemName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetImgResID() throws Exception {
+
+    }
+}
