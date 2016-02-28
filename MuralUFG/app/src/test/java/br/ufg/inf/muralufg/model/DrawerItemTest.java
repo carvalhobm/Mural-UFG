1e2238b3ea5303716ebdc9b8d5eb69f051c95fca
LucasFerreiraRodrigue <lucas_f_rodrigues@hotmail.com>
2016-02-28 18:43:18 -0300

    Criação dos teste da classe DrawerItem

--- MuralUFG/app/src/test/java/br/ufg/inf/muralufg/model/DrawerItemTest.java
+++ MuralUFG/app/src/test/java/br/ufg/inf/muralufg/model/DrawerItemTest.java
@@ -1,27 +1,31 @@
 package br.ufg.inf.muralufg.model;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Bruno on 28/02/2016.
+ * Created by Lucas Ferreira on 28/02/2016.
  */
 public class DrawerItemTest {
 
+    DrawerItem drawerItem;
+
     @Before
     public void setUp() throws Exception {
-
+        drawerItem = new DrawerItem("itemName", 1);
     }
 
     @Test
     public void testGetItemName() throws Exception {
-
+        Assert.assertEquals("itemName", drawerItem.getItemName());
     }
 
     @Test
     public void testGetImgResID() throws Exception {
-
+        Assert.assertEquals(1, drawerItem.getImgResID());
     }
 }
